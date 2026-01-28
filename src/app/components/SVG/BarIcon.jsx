21036eddb7bf0dd663408928ccee9bfa06fb369c
Hrithik Biswas <mr.hrithikbiswas@gmail.com>
2026-01-29 03:01:08 +0600

    feat: add BarIcon component and integrate MobileMenu into PageHeader

---
+++ src/app/components/SVG/BarIcon.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const BarIcon = () => {
+    return (
+        <svg
+            className="w-14 h-14 text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeWidth="2"
+                d="M5 7h14M5 12h14M5 17h14"
+            />
+        </svg>
+    );
+};
+
+export default BarIcon;
--- src/app/components/layout/PageHeader.jsx
+++ src/app/components/layout/PageHeader.jsx
@@ -1,4 +1,6 @@
+'use client';
 import Image from 'next/image';
+import MobileMenu from './MobileMenu';
 
 const PageHeader = ({ title }) => {
     return (
@@ -6,21 +8,22 @@ const PageHeader = ({ title }) => {
             <h1 className="text-white text-2xl sm:text-[32px] font-semibold ml-4 sm:ml-6">
                 {title}
             </h1>
-            <div className="flex items-center space-x-3 sm:space-x-10 mr-4 sm:mr-6">
+            <div className="flex items-center gap-x-3 sm:gap-x-6 md:gap-x-10 mr-4 sm:mr-6">
                 <Image
                     src="/notifications.png"
                     alt="Search"
                     width={32}
                     height={32}
-                    className=""
+                    className="hidden sm:inline"
                 />
                 <Image
                     src="/Avatar.png"
                     alt="Search"
                     width={80}
                     height={80}
-                    className="rounded-full w-15 sm:w-20 h-15 sm:h-20"
+                    className="hidden sm:inline rounded-full w-15 sm:w-20 h-15 sm:h-20"
                 />
+                <MobileMenu />
             </div>
         </div>
     );
